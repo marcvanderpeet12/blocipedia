3da60c66b8686d5d76787e8dd592907b233740ab
Marc van der Peet <marcvanderpeet@gmail.com>
2015-02-20 11:01:57 +0000

    changes collaboration model - working

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -3,6 +3,19 @@ class CollaborationsController < ApplicationController
   def new 
     @wiki = Wiki.find(params[:wiki_id])
     @collaboration = @wiki.collaborations.new
+    @users = User.all
   end
+
+  def create
+    @wiki = Wiki.find(params[:wiki_id])
+    #selected users
+    @collaboration = @wiki.collaborations.build(user_id: params[:user_id])
+    if @collaboration.save
+      redirect_to wikis_path, notice: "Wiki shared."
+    else
+      flash[:error] = "Error creating wiki. Try again."
+      render :new
+  end
+end
 
 end
